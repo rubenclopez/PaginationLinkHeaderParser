6f48216a0a0e1e44fb17cb7d84800a3f1e34eabf
Rubén López <ruben@cs1.com>
2014-04-13 23:20:43 -0600

    Removed example from class file.

--- link_header.rb
+++ link_header.rb
@@ -26,11 +26,3 @@ class LinkHeader
     to_h[rel.to_s]
   end
 end
-
-# Example
-
-links_header_string = '<https://api.github.com/user/repos?page=4&per_page=100>; rel="next", <https://api.github.com/user/repos?page=50&per_page=100>; rel="last", <https://api.github.com/user/repos?page=1&per_page=100>; rel="first", <https://api.github.com/user/repos?page=2&per_page=100>; rel="prev"'
-
-links = LinkHeader.parse(links_header_string)
-
-p links.to_h
